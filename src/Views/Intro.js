70330e3e80c4d415cfd0f8fd9e22513d023cd75b
egolfc <cae5511@gmail.com>
2021-03-17 09:34:31 -0400

    added more information and corrected some formatting. added OHIO file for additional information about what I do with the organization

--- src/Views/Intro.js
+++ src/Views/Intro.js
@@ -2,37 +2,61 @@ import "../App.css";
 import React, { Component } from "react";
 import { Header, Container, Divider, Segment, Image } from "semantic-ui-react";
 import osu from "../assets/images/osu.jpg";
+import ohio from "../assets/images/OHIO-subtitle.png";
+import profile from "../assets/images/profile1.JPG";
 
 function IntroSegment() {
   return (
     <div>
       <Container className="welcome-container" textAlign="left">
-        <Header inverted size="large" color="grey">
-          Welcome!!
-        </Header>
+        <Container className="welcome-header">
+          <Header inverted color="grey">
+            Welcome!!
+          </Header>
+        </Container>
         <Divider />
         <Container class="intro-container">
-          <Header inverted size="small" color="grey">
+          <Header inverted color="grey">
             My name is Christopher Egolf and I am an aspiring web developer who
             loves to work with React and AWS.
           </Header>
+          <Image
+            className="profile-img"
+            size="medium"
+            src={profile}
+            floated="right"
+          />
         </Container>
         <Container className="about-me-container">
-          <Header size="medium" inverted color="grey">
+          <Header size="huge" inverted color="grey">
             About me
           </Header>
-          <Segment inverted className="about-me-segment">
-            <Container className="about-me-text">
-              I am a junior college student studying computer and information
-              science at The Ohio State University. I am most interested in web
-              developement and DevOps as well as cyprtography and information
-              security.
-            </Container>
+          <Image src={osu} floated="right" size="medium" />
 
-            <Container className="osu-image-container">
-              <Image src={osu} floated="right" size="medium" />
+          <Container className="about-me-text">
+            I am a junior college student studying computer and information
+            science at The Ohio State University. I am most interested in web
+            developement and DevOps as well as cyprtography and information
+            security.
+          </Container>
+          <Container className="activities-container">
+            <Header size="huge" inverted color="grey">
+              Activities
+            </Header>
+            <Container className="activities-text">
+              I am a member of the OHI/O student organization at OSU. OHI/O is
+              an organization that plans and puts on HackOHI/O, OSU's hackathon.
+              We also put on other events such as MAKE OHI/O and High School
+              I/O.
             </Container>
-          </Segment>
+          </Container>
+          <Image
+            className="OHIO-img"
+            size="medium"
+            src={ohio}
+            floated="right"
+          />
+          <Divider />
         </Container>
       </Container>
     </div>
---
+++ src/Views/OHIO.js
@@ -0,0 +1,9 @@
+import "../App.css";
+import React, { Component } from "react";
+import { Header, Container, Divider, Segment, Image } from "semantic-ui-react";
+
+function OHIO() {
+  return <div></div>;
+}
+
+export default OHIO;
